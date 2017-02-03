c9707042f584e5ff9c78403878a88d18fcb55c5d
Andy Pieters <andy@pay.nl>
2017-02-03 17:31:49 +0100

    Updated version number

--- Frontend/PaymentPayNL/Bootstrap.php
+++ Frontend/PaymentPayNL/Bootstrap.php
@@ -14,7 +14,7 @@ class Shopware_Plugins_Frontend_PaymentPayNL_Bootstrap extends Shopware_Componen
      */
     public function getVersion()
     {
-        return '2.0.1';
+        return '2.0.2';
     }
 
     /**
